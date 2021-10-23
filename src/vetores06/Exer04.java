80f9780bd2a1306c456550e175a4fb282a3baf6d
MateusSebold <91922068+MateusSebold@users.noreply.github.com>
2021-10-22 23:35:28 -0300

    Atualização

---
+++ src/vetores06/Exer04.java
@@ -0,0 +1,22 @@
+package vetores06;
+import java.util.Scanner;
+public class Exer04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		float soma = 0;
+		for (int i=1;i<=4;i++) {
+			
+			System.out.println("Digite a nota");
+			float nota = entrada.nextFloat();
+			System.out.println(nota);
+			soma = soma +nota;
+		}
+		System.out.println("A média é "+soma/4);
+		entrada.close();
+		
+		
+	}
+
+}
